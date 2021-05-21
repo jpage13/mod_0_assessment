8294008aefa556adb3fc109ee6e5374dc99b8bdc
Jimmy Page <jpage@knights.ucf.edu>
2021-05-21 16:22:59 -0600

    Initial commit

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+HOUSE = {
+"purpose" => 'helps protect me from weather',
+"ability" => 'keeps me warm',
+"square_feet" => 2000,
+"number_of_bedrooms" => 5,
+"is_modern" => true,
+"is_one_story" => true,
+"room_types" => ['bedrooms','kitchen','bathrooms'],
+"activities" => ['eating','sleeping','working']
+}
