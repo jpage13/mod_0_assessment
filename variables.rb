25bbf2fab612c91e64c4d27feb0f97a4620579a8
Jimmy Page <jpage@knights.ucf.edu>
2021-05-21 15:54:11 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+animal = "lives in the woods"
+is_cuddly = true
+animal = "lives in your house"
+num_8 = 8
+num_3 = 3
+quotient = (num_8).div/(num_3)
+favorite_films = ['Road Trip','Up','Deja Vu']
+favorite_films.shift
+animal.length()
