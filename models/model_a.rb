12d79bd1e2f050696ac59ea444c3eb1f1b5808f1
Jimmy Page <jpage@knights.ucf.edu>
2021-05-21 16:15:26 -0600

    Initial commit

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+LAPTOP = {
+"purpose" => 'helps me with my homework',
+"ability" => 'connects me with coworkers',
+"weight_in_lbs" => 7,
+"number_of_keys" => 57,
+"is_apple" => true,
+"gets_hot" => true,
+"browsers" => ['Chrome','Firefox','Safari'],
+"apps" => ['Garageband','Music','Calendar']
+}
